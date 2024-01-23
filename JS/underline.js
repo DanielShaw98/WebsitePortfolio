13b5b379b548192dd32fe91daa138f6eb386ff17
Daniel Shaw <danielshaw71@gmail.com>
2024-01-23 17:45:17 +0000

    Underline animation working, could improve

---
+++ JS/underline.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const underline = document.querySelector('.underline');
+
+  function extend() {
+    underline.style.transition = 'width 0.5s ease-in-out';
+    underline.style.width = '500px';
+  }
+
+  function revert() {
+    underline.style.transition = 'width 0.5s ease-in-out';
+    underline.style.width = '300px';
+  }
+
+    setTimeout(function () {
+      extend();
+      setTimeout(revert, 500);
+    }, 250);
+});
